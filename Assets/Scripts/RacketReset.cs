1775234b24e93021cb9dacc2429ee9afd83deeb6
Koeko314 <konstantin.koefler@stud.fh-campuswien.ac.at>
2025-12-08 13:51:49 +0100

    Added a Reset for the Racket flying away

---
+++ Assets/Scripts/RacketReset.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class RacketReset : MonoBehaviour
+{
+    Vector3 RESETPOS = new Vector3(0,2,0);
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Racket")
+        {
+            other.gameObject.transform.position = RESETPOS;
+            other.gameObject.GetComponent<Rigidbody>().linearVelocity = new Vector3(0, 0, 0);
+            other.gameObject.GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, 0);
+            other.gameObject.transform.rotation = Quaternion.identity;
+        }
+    }
+}
